5affb4d98996ab3ce3578cedaef626af20a2ebce
louai <louaibaghdadi27@gmail.com>
2023-09-12 15:00:46 +0300

    EX1

---
+++ LouaiBaghdadi.js
@@ -0,0 +1,12 @@
+const numberKey=prompt("enter the number key");
+
+switch(numberKey) {
+    case "0":
+      console.log("we sank together");
+      break;
+    case "30000":
+      console.log("we made it");
+      break;
+      default:
+        console.log("we need to swim a little bit more!")
+  }
