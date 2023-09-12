d04562c6779bdc8457495c58a386e164a0ef3307
louai <louaibaghdadi27@gmail.com>
2023-09-12 15:24:26 +0300

    EX2

--- LouaiBaghdadi.js
+++ LouaiBaghdadi.js
@@ -1,12 +1,24 @@
-const numberKey=prompt("enter the number key");
+// const numberKey=prompt("enter the number key");
 
-switch(numberKey) {
-    case "0":
-      console.log("we sank together");
-      break;
-    case "30000":
-      console.log("we made it");
-      break;
-      default:
-        console.log("we need to swim a little bit more!")
-  }
+// switch(numberKey) {
+//     case "0":
+//       console.log("we sank together");
+//       break;
+//     case "30000":
+//       console.log("we made it");
+//       break;
+//       default:
+//         console.log("we need to swim a little bit more!")
+//   }
+
+const array=[1,2,8,4,7,6];
+
+var max=array[0];
+
+
+for(let i=1;i<array.length;i++){
+    if(array[i] > max){
+        max =array[i];
+    } 
+}
+console.log(max);
