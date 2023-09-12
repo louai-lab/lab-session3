c6faa9e9e301bca21e5408fbf84938d305335e82
louai <louaibaghdadi27@gmail.com>
2023-09-12 15:35:50 +0300

    EX2

--- LouaiBaghdadi.js
+++ LouaiBaghdadi.js
@@ -1,3 +1,6 @@
+
+/*      exercice 1   */ 
+
 // const numberKey=prompt("enter the number key");
 
 // switch(numberKey) {
@@ -11,14 +14,23 @@
 //         console.log("we need to swim a little bit more!")
 //   }
 
+
+/*      exercice 2   */ 
+
 const array=[1,2,8,4,7,6];
 
 var max=array[0];
+
+var min=array[0];
 
 
 for(let i=1;i<array.length;i++){
     if(array[i] > max){
         max =array[i];
     } 
+    else if(array[i < min]){
+        min =array[i];
+    }
 }
 console.log(max);
+console.log(min);
